1aef304e2394f3ce6c306064808334f821942ccd
李志敏 <915377975@qq.com>
2019-08-27 00:40:41 +0800

    test github

--- demo/src/main/java/demo1/testthread.java
+++ demo/src/main/java/demo1/testthread.java
@@ -3,5 +3,11 @@ package demo1;
 public class testthread {
     public static void main(String[] args) {
 
+        for (int i = 0; i <5 ; i++) {
+            if (i<8){
+                System.out.println(i);
+            }
+        }
+
     }
 }
